0d960a7433d81241b980214d139ea195119d29f0
nickkreissler <nick.d.kreissler@gmail.com>
2018-01-16 08:45:35 -0600

    update

--- homework/homework1/[kreissler]asg1.py
+++ homework/homework1/[kreissler]asg1.py
@@ -1,3 +1,6 @@
+#Nick Kreissler
+
+
 class transportcenter(object):
 
     def __init__(self,numunits,uin,uout,spaces):
@@ -18,25 +21,26 @@ class transportcenter(object):
         self.uin += unitsIn
         self.spaces -= unitsIn
         if self.spaces < 0:
-            print('to many units...')
+           # print('to many units...')
+            pass
 
     def unitsOut(self,numOut):
         if numOut > self.numunits:
-            print("Can't have more units out than in")
+            print("Can't have more units out than in!")
         else:
             self.numunits -= numOut
             self.uout+=numOut
             self.spaces += numOut
 
     def unitsHere(self):
-        print(self.numunits)
+        return self.numunits
 
     def unitsPrint(self):
         print("Total units in {}, Total units out {}".format(self.uin,self.uout))
 
     def unitCheck(self):
-        print("Units here now {}".format(self.numunits))
-        print("Units at start {} + units in {} - units out {} = {}".format(self.beginningunits,self.uin,self.uout,self.numunits))
+        return "Sanity Check Passed \nUnits here now: {} \nUnits at start {} + units in {} - units out {} = {}".format(self.numunits, self.beginningunits,self.uin,self.uout,self.numunits)
+
 
 class airport(transportcenter):
 
@@ -54,7 +58,29 @@ class airport(transportcenter):
             self.runways = 0
 
     def unitsPrint(self):
-        print("Total units in: {}, Total units out: {} Total Runways: {}".format(self.uin,self.uout, self.runways))
+        print("Total units in: {}, Total units out: {}, Total Runways: {}".format(self.uin,self.uout, self.runways))
 
 
+
+otc = transportcenter(0,0,0,100)
+otc.unitsIn(100)
+print(otc.uin)
+otc.unitsOut(1000)
+otc.unitsOut(10)
+print(otc.unitsHere())
+otc.unitsPrint()
+print(otc.unitCheck())
+print("\n")
+
+myap = airport(0,0,0,0)
+myap.addRunways(100)
+myap.unitsPrint()
+myap.remRunways(10)
+myap.unitsPrint()
+print(myap.unitsHere())
+myap.unitsIn(20)
+myap.unitsOut(100)
+myap.unitsOut(10)
+print(myap.unitCheck())
+print(myap.unitsHere())
 
